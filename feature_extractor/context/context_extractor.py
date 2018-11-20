eb2a12a57194141200ec9776976858e131d68e05
scotthou <sh3658@columbia.edu>
2018-11-20 14:31:22 -0500

    change indentation to spaces

--- feature_extractor/context/context_extractor.py
+++ feature_extractor/context/context_extractor.py
@@ -3,20 +3,20 @@ from tensorflow.keras.applications import VGG16
 import numpy as np
 
 class ContextExtractor:
-	def __init__(self):
-		# Use the first six layers of vgg16
-		convbase = VGG16(weights='imagenet',
-			             include_top=True)
-		layers = convbase.layers[:21]
-		self.model = tf.keras.models.Sequential()
+    def __init__(self):
+        # Use the first six layers of vgg16
+        convbase = VGG16(weights='imagenet',
+                         include_top=True)
+        layers = convbase.layers[:21]
+        self.model = tf.keras.models.Sequential()
 
-		for layer in layers:
-			self.model.add(layer)
+        for layer in layers:
+            self.model.add(layer)
 
-	def __call__(self, data):
-		# data is a RGB numpy array represents an image
-		# Should have size (224,224,3)
-		# Return the feature vector of shape (4096,)
-		assert data.shape == (224,224,3)
-		data = np.array([data])
-		return self.model.predict(data)[0]
+    def __call__(self, data):
+        # data is a RGB numpy array represents an image
+        # Should have size (224,224,3)
+        # Return the feature vector of shape (4096,)
+        assert data.shape == (224,224,3)
+        data = np.array([data])
+        return self.model.predict(data)[0]
