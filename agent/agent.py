96e674e87022e6b18de5b22e6037d186ef99ef1c
scotthou <sh3658@columbia.edu>
2018-11-20 22:38:24 -0500

    Skeleton code for agent and replay buffer

---
+++ agent/agent.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def step(self):
+        pass
+
+    def act(self):
+        pass
+
+    def learn(self):
+        pass
+
+    def soft_update(self):
+        pass
+
+class ReplayBuffer:
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def sample(self):
+        pass
+
+    def __len__(self):
+        pass
