9e4ede8b2f7f43b5d305cc7a440013dfba70d0aa
Daniel Bu <danielbu@Daniel-Bus-MBP.fios-router.home>
2018-11-27 00:45:08 -0500

    Add normalization

--- feature_extractor/color_histogram/color_histogram.py
+++ feature_extractor/color_histogram/color_histogram.py
@@ -5,6 +5,7 @@
 
 import sys
 import cv2  # OpenCV library
+import numpy as np
 # need this code below to avoid some strange error of matplotlib on Mac
 if sys.platform == 'darwin':
     import matplotlib
@@ -20,12 +21,25 @@ def get_histogram(img):
     histogram = []   # L, a, b in bins for histogram
     for i in range(3):
         histogram.append(cv2.calcHist([equalized_img], [i], None, [32], [0,256]))   # 32 bins
-    return histogram
+    return _normalize(histogram)
+
+# use formula: normalized_x = (x - min) / (max - min)
+def _normalize(histogram):
+    normalized_histogram = np.array([np.zeros((32, 1)), np.zeros((32, 1)), np.zeros((32, 1))])
+    for i in range(3):
+        maxv = np.max(histogram[i])
+        minv = np.min(histogram[i])
+        for j in range(32):
+            normalized_histogram[i][j] = (histogram[i][j] - minv) / (maxv - minv)
+    return normalized_histogram
+
 ''' # test client
 if __name__ == '__main__':
     img_path = sys.argv[1]
     img = cv2.imread(img_path, 1) # load a color image in np.array
+    #img = np.random.randint(256, size=(200,200,3), dtype='uint8')
     hg = get_histogram(img)
+    print(type(hg))
     color = ['r', 'g', 'b'] # L = red, a = green, b = blue
     for i, c in enumerate(color):
         plt.plot(hg[i], color = c)
